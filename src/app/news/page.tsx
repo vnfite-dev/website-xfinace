8b4a78fbe0b2c17221352e8525589ba099fab2f8
chuvannam832002 <chuvannam70@gmail.com>
2025-07-29 15:10:54 +0700

    xoa bo du lieu thua trong news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -8,15 +8,6 @@ export default function NewsPage() {
     <HeaderImageLayout imageHead="/images/support/support-header.png">
       <FeatureNews />
       <XFinaceNews />
-      {/* <div className="max-w-[1280px] w-full h-full flex flex-col items-start justify-start gap-2 mx-auto mb-10">
-      <div className="bg-red w-full">
-      <p className="font-bold text-lg sm:text-2xl md:text-4xl leading-6 md:leading-14 text-center mt-4 sm:mt-8 lg:mt-12">
-          <span className="text-main-color">X-Finance</span>{" "}
-          và khách hàng
-        </p>
-        <YoutubeShort />
-      </div>
-      </div> */}
     </HeaderImageLayout>
   );
 }
